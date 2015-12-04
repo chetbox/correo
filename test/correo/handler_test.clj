ccc54fdfc4fa38c5917c2105daaba5b76f9128ec
Chetan Padia <chetbox@gmail.com>
2015-12-04 13:40:57 +0000

    fix tests

--- test/correo/handler_test.clj
+++ test/correo/handler_test.clj
@@ -3,12 +3,14 @@
             [ring.mock.request :as mock]
             [correo.handler :refer :all]))
 
+(def app-example (app (read-config "config/email.example.edn")))
+
 (deftest test-app
   (testing "/"
-    (let [response (app-with-default-config (mock/request :get "/"))]
+    (let [response (app-example (mock/request :get "/"))]
       (is (= (:status response) 200))
       (is (= (:body response) "Hello Email"))))
 
   (testing "not-found route"
-    (let [response (app-with-default-config (mock/request :get "/invalid"))]
+    (let [response (app-example (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
